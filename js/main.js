7c23673b4a917555b28292835aafcd4df8b1f05c
kim <jayfulkr@gmail.com>
2023-06-12 23:50:12 +0900

    06/12 #6

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const leaflet = document.querySelector('.leaflet');
-// const page = document.querySelector('.page');
+const pageElems = document.querySelectorAll('.page');
+let pageCount = 0;
 
 function getTarget(ele, className) {
   while (!ele.classList.contains(className)) {
@@ -12,9 +13,38 @@ function getTarget(ele, className) {
   return ele;
 }
 
+function closeLeaflet() {
+  pageCount = 0;
+  document.body.classList.remove('leaflet-opened');
+  pageElems[2].classList.remove('page-flipped');
+  setTimeout(() => {
+    pageElems[0].classList.remove('page-flipped');
+  }, 500);
+}
+
+function zoomIn(elem) {
+  const rect = elem.getBoundingClientRect();
+  const dx = innerWidth / 2 - (rect.x + rect.width / 2);
+  const dy = innerHeight / 2 - (rect.x + rect.height / 2);
+  leaflet.style.transform = `translate3d(${dx}px, ${dy}px, 50vw)`;
+  console.log(leaflet);
+}
+
 leaflet.addEventListener('click', e => {
   let pageEle = getTarget(e.target, 'page');
   if (pageEle) {
-    pageEle.classList.add('page-filpped');
+    pageEle.classList.add('page-flipped');
+    pageCount++;
+    if (pageCount === 2) {
+      document.body.classList.add('leaflet-opened');
+    }
+  }
+  let closeBtnEle = getTarget(e.target, 'close-btn');
+  if (closeBtnEle) {
+    closeLeaflet();
+  }
+  let menuItemElem = getTarget(e.target, 'menu-item');
+  if (menuItemElem) {
+    zoomIn(menuItemElem);
   }
 });
