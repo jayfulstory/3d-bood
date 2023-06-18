49ebe6d3ea72e76a9deed9a50b4862c248aea32f
kim <jayfulkr@gmail.com>
2023-06-18 21:57:17 +0900

    06/18 #8

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const leaflet = document.querySelector('.leaflet');
 const pageElems = document.querySelectorAll('.page');
 let pageCount = 0;
+let currentMenu;
 
 function getTarget(ele, className) {
   while (!ele.classList.contains(className)) {
@@ -25,9 +26,35 @@ function closeLeaflet() {
 function zoomIn(elem) {
   const rect = elem.getBoundingClientRect();
   const dx = innerWidth / 2 - (rect.x + rect.width / 2);
-  const dy = innerHeight / 2 - (rect.x + rect.height / 2);
-  leaflet.style.transform = `translate3d(${dx}px, ${dy}px, 50vw)`;
-  console.log(leaflet);
+  const dy = innerHeight / 2 - (rect.y + rect.height / 2);
+
+  let angle;
+
+  switch (elem.parentNode.parentNode.parentNode.dataset.page * 1) {
+    case 1:
+      angle = -30;
+      break;
+    case 2:
+      angle = 0;
+      break;
+    case 3:
+      angle = 30;
+      break;
+  }
+
+  document.body.classList.add('zoomIn');
+  leaflet.style.transform = `translate3d(${dx}px, ${dy}px, 50vw) rotateY(${angle}deg`;
+  currentMenu = elem;
+  currentMenu.classList.add('current-menu');
+}
+
+function zoomOut() {
+  leaflet.style.transform = 'translate3d(0,0,0)';
+  if (currentMenu) {
+    document.body.classList.remove('zoomIn');
+    currentMenu.classList.remove('current-menu');
+    currentMenu = null;
+  }
 }
 
 leaflet.addEventListener('click', e => {
@@ -46,5 +73,11 @@ leaflet.addEventListener('click', e => {
   let menuItemElem = getTarget(e.target, 'menu-item');
   if (menuItemElem) {
     zoomIn(menuItemElem);
+  }
+
+  let backBtn = getTarget(e.target, 'back-btn');
+  if (backBtn) {
+    console.log(backBtn);
+    zoomOut();
   }
 });
