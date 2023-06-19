bc2f02c11ced0e7e40147cbafc7c830de75a8a61
kim <jayfulkr@gmail.com>
2023-06-19 22:44:18 +0900

    06/19 #9

--- js/main.js
+++ js/main.js
@@ -69,10 +69,14 @@ leaflet.addEventListener('click', e => {
   let closeBtnEle = getTarget(e.target, 'close-btn');
   if (closeBtnEle) {
     closeLeaflet();
+    zoomOut();
   }
+
   let menuItemElem = getTarget(e.target, 'menu-item');
   if (menuItemElem) {
-    zoomIn(menuItemElem);
+    if (!document.body.classList.contains('zoomIn')) {
+      zoomIn(menuItemElem);
+    }
   }
 
   let backBtn = getTarget(e.target, 'back-btn');
@@ -81,3 +85,30 @@ leaflet.addEventListener('click', e => {
     zoomOut();
   }
 });
+
+const hand = document.querySelector('.hand');
+const handPos = {
+  x: 0,
+  y: 0,
+};
+const targetPos = {
+  x: 0,
+  y: 0,
+};
+
+function render() {
+  distX = targetPos.x - handPos.x;
+  distY = targetPos.y - handPos.y;
+  handPos.x = handPos.x + distX * 0.1;
+  handPos.y = handPos.y + distY * 0.1;
+
+  hand.style.transform = `translate(${handPos.x}px, ${handPos.y}px)`;
+  requestAnimationFrame(render);
+}
+render();
+
+// window.addEventListener('mousemove', e => {
+//   targetPos.x = e.client.x;
+//   targetPos.y = e.clientY;
+//   hand.style.transform = `translate(${e.clientX - 50}px, ${e.clientY}px)`;
+// });
